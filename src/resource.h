a9f757cd68e04e0740708c4d9154bcaea0ced395
dualface <dualface@gmail.com>
2026-02-17 21:11:39 +0800

    feat: add VERSIONINFO resource with file/product version metadata

--- src/resource.h
+++ src/resource.h
@@ -4,3 +4,12 @@
 // IDI_APPICON_LIGHT: Dark-on-light icon (for light mode / light taskbar)
 #define IDI_APPICON 101
 #define IDI_APPICON_LIGHT 102
+
+// Version info
+#define VER_MAJOR 1
+#define VER_MINOR 0
+#define VER_PATCH 0
+#define VER_BUILD 0
+
+#define VER_FILE_STR    "1.0.0.0"
+#define VER_PRODUCT_STR "1.0.0"
